9e13ef5fc5177275b7ae48301192b57fc83a4cd6
suraz343 <prajapati.suraj343@gmail.com>
2025-01-28 16:21:43 +0530

    Initial Commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-gray-50 dark:bg-gray-900 transition-colors duration-300">
+      
+    </main>
+  )
+}
